230c2184856a4c89ad5094db2d0faec1b2ed7e08
cho237 <kinslycho237@gmail.com>
2024-02-21 13:22:11 +0100

    adding home page and upcoming movies widget

---
+++ lib/screens/home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/widgets/upcoming.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 18, horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hello Alex",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          "What to Watch?",
+                          style: TextStyle(
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Image.asset(
+                        "images/p.png",
+                        height: 60,
+                        width: 60,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 60,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF292B37),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.search,
+                      color: Colors.white54,
+                      size: 30,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 5),
+                      width: 300,
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search",
+                          hintStyle: TextStyle(
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              const UpComing()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
